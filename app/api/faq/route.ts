788b10209ac496fc78b2449f57643950c4d62298
Sooho <sooho.lim4@gmail.com>
2025-05-04 22:05:14 -0700

    lint

--- app/api/faq/route.ts
+++ app/api/faq/route.ts
@@ -1,11 +1,7 @@
 import { asc, eq } from "drizzle-orm";
 import { faq } from "@/drizzle/schema";
 import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
-import { NextResponse } from "next/server";
-
-const pool = mysql.createPool(process.env.DATABASE_URL!);
-const db = drizzle({ connection: pool });
+import { NextResponse } from "next/server"
 
 export const runtime = "nodejs";
 export async function GET() {
