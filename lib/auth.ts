b2f7f3db2566dba6e59832dd04fed3fc847b38ae
Sooho <sooho.lim4@gmail.com>
2025-05-04 20:00:43 -0700

    trust

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,5 +3,6 @@ import Discord from "next-auth/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [Discord],
 });
